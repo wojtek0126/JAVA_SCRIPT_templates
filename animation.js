537e0313bee4672f467c021ffd3ec322bc48b183
wojtek0126 <wojtek0126@gmail.com>
2020-10-07 20:00:22 +0200

    templatki opis w nazwach plików/templates described by filenames

--- animation.js
+++ animation.js
@@ -7,6 +7,10 @@ print('tet');
 let paintbox = document.getElementById('paintbox');
 
 let context = paintbox.getContext('2d');
+
+let gameOn = true;
+
+let plyerSpeed = 5;
 // context.moveTo();
 // context.lineTo();
 // context.stroke();
@@ -33,6 +37,13 @@ class Player extends Box {
         super(50,'blue');
         this.x = 0;
         this.y = 0;
+        this.speed = 0;
+        // this.moving = false;
+    }
+    move() {
+        // if(this.moving) {
+        this.x += this.speed;
+        // }
     }
 }
 //enemy inherits x and y from player
@@ -41,9 +52,9 @@ class Enemy extends Box {
         super(50,'red');
         this.speed = speed;
     }
-    move() {
+    move() {    //this bounces objects off canvas walls
         this.y += this.speed;
-        if (this.y + this.size > 500) {
+        if (this.y + this.size > 500) {  //500 is canvas siza here
             this.speed = -(Math.abs(this.speed));
         }
         if (this.y < 0) {
@@ -58,23 +69,61 @@ let e2 = new Enemy(2);
 e1.x = 120;
 e2.x = 240;
 //drawbox creates player or enemy etc they are visible when speed is determined
+function isCollided(box1, box2) {
+
+}
+
 function drawbox(box) {
     context.fillStyle = box.color;
     context.fillRect(box.x, box.y, box.size, box.size);
 }
 
+paintbox.addEventListener('mousedown', () => {
+    print(plyerSpeed, ' player speed');
+    player.speed = 5;
+})
 
-// below instead of interval
-function updateGame() {
-    window.requestAnimationFrame(() => {
-        print('update frame');
-        context.clearRect(0, 0, 500, 500);
-        e1.move();
-        e2.move();
-        drawbox(e1);
-        drawbox(e2);
-        updateGame();
-    })
+paintbox.addEventListener('mouseup', () => {
+    player.speed = 0;
+})
+
+setInterval(() => {
+    plyerSpeed = 5 + parseInt(Math.random() * 10);
+    player.y = 100 + (Math.random() * 300);
+}, 2000);
+
+function gameloop() {
+    if (!gameOn) return
+
+
+    print('update frame');
+    context.clearRect(0, 0, 500, 500);  //this is relative to canvas size
+    e1.move();  //this adds move
+    e2.move();
+    player.move();
+
+    if (isCollided(e1, player) || isCollided(e2, player) ) {
+        window.alert('game over');
+    }
+    drawbox(player); //this just draws static
+    drawbox(e1);
+    drawbox(e2);
+    window.requestAnimationFrame(gameloop);
 }
+// below instead of interval
+// function updateGame() {
+//     window.requestAnimationFrame(() => {
+//
+//         updateGame();
+//     })
+// }
 
-updateGame();
+gameloop();
+
+
+
+// setInterval(() => {
+//
+// }, 100);
+
+//drawbox(e1) to show it by now
