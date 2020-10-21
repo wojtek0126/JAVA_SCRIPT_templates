766bc786cc567be0cf61e9906f79f1602ed33a63
wojtek0126 <wojtek0126@gmail.com>
2020-10-21 16:45:51 +0200

    templates JS

--- javaScript/delete_element_from_list_on_click.js
+++ javaScript/delete_element_from_list_on_click.js
@@ -1,6 +1,7 @@
 //list of elements is base and target is a button. This is for propagation, when just button is selected to click,
 //it will bug on last element as null
 
+//if delete button is within list element
 const deleteElemEnabled = (listOfElements, classOfDeleteButton, elementToDelete) => {
     listOfElements.addEventListener('click', function (e) {
         const elements = document.querySelector(elementToDelete);
@@ -14,3 +15,9 @@ const deleteElemEnabled = (listOfElements, classOfDeleteButton, elementToDelete)
     })
 }
 
+//if delete button is outside, deleting last element in this case, it is example not function
+deleteLastElBtn.addEventListener("click", () => {
+    let listElems = document.getElementsByClassName('list-group-item');
+    let lastElemOfList = listElems[listElems.length - 1];
+    if (listElems.length > 0) listElems[listElems.length - 1].remove();
+});
