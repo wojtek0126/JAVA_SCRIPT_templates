155ad157e09148982198e4a4959b9f095c1d2630
wojtek0126 <wojtek0126@gmail.com>
2020-10-21 16:43:08 +0200

    templates JS

--- javaScript/function_random_in_range_of_numbers.js
+++ javaScript/function_random_in_range_of_numbers.js
@@ -1,3 +1,8 @@
 const random = (min, max) => {
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
+
+//lub range from 1 to selected number
+const random = (rangeFrom1) => {
+    return Math.floor(Math.random() * rangeFrom1);
+}
