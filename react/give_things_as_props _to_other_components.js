d8cd7be6612eb042fc6d8a49f8c1ea93b230159c
wojtek0126 <wojtek0126@gmail.com>
2020-10-23 21:11:05 +0200

    templates JS

--- react/give_things_as_props _to_other_components.js
+++ react/give_things_as_props _to_other_components.js
@@ -1,1 +1,68 @@
-const {min, max, count, array} = props;
+//numbers from <App min={3} max={6} count={9}/> are being caught by const {min, max, count} = this.props;
+// and passed upwards by another const {min, max, count} = this.props;
+
+
+import React, {Component} from "react";
+import ReactDOM from "react-dom";
+
+const print = (element) => {
+    console.log(element);
+}
+
+class RandomNumbers extends Component {
+    render() {
+        const array = [];
+        const {min, max, count} = this.props;
+        const random = (min, max) => {
+            return Math.floor(Math.random() * (max - min + 1) + min);
+        }
+        for (let c = 1;c <= count;c++) {
+            const result = random(min, max);
+            print(result);
+            array.push(result);
+        }
+        print(array);
+        return array.map(element => <span>{element} </span> );
+    }
+}
+//
+class RandomNumbersInfo extends Component {
+    render() {
+        const {min, max, count} = this.props;
+
+
+        return (
+            <>
+            <h1>{min}</h1>
+            <h1>{max}</h1>
+            <h1>{count}</h1>
+            </>
+    )
+    }
+}
+
+class RandomNumbersList extends Component {
+    render() {
+        const {min, max, count} = this.props;
+        return (
+            <RandomNumbers min={min} max={max} count={count} />
+    )
+    }
+}
+
+class App extends Component {
+    render() {
+        const {min, max, count} = this.props;
+        return (
+            <>
+            <RandomNumbersList min={min} max={max} count={count}/>
+        <RandomNumbersInfo min={min} max={max} count={count}/>
+        </>
+    )
+    }
+}
+
+ReactDOM.render(
+<App min={3} max={6} count={9}/>,
+document.getElementById("app")
+);
