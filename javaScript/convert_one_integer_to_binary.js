d88899d126a3885dfd931b1f09ef268f43725c2b
wojtek0126 <wojtek0126@gmail.com>
2020-10-23 19:42:18 +0200

    JS templates

--- javaScript/convert_one_integer_to_binary.js
+++ javaScript/convert_one_integer_to_binary.js
@@ -1,3 +1,15 @@
+
 const countBits = number => {
     return (number >>> 0).toString(2);
+}
+
+
+//or
+function addBinary( a, b ){
+    let sum = a + b,
+        binary = '';
+    while ( sum > 0 ) {
+        binary = ( sum % 2 ) + binary;
+        sum = Math.floor( sum / 2 );
+    } return binary;
 }
