a692c4cbc811268b04954196b21fad21480fd7e0
LionbridgeOffice <jakub.golebiewski@lionbridge.com>
2022-01-24 10:56:21 +0100

    get object values

---
+++ javaScript/get_values_from_object.js
@@ -0,0 +1,6 @@
+const object = {a: 2, b: 3, c: 4}
+const values = Object.values(object) //2, 3, 4
+// also for loop:
+//  for (number in object) {
+//     if (object[number] === 3) return "yes" 
+// }
